4d2c890811507cc6363b90dca529d5957bb7ceb4
Mike Harder <mharder@microsoft.com>
2018-05-09 17:31:53 -0700

    console

--- AspNetCoreSdkTests/TemplateTests.cs
+++ AspNetCoreSdkTests/TemplateTests.cs
@@ -92,6 +92,7 @@ namespace AspNetCoreSdkTests
             // Self-contained, osx-x64
             // ClassLibrary does not require a package source, even for self-contained deployments
             Template.GetInstance<ClassLibraryTemplate>(NuGetPackageSource.None, RuntimeIdentifier.OSX_x64),
+            Template.GetInstance<ConsoleApplicationTemplate>(NuGetPackageSource.NuGetOrg, RuntimeIdentifier.OSX_x64),
         };
 
         public static IEnumerable<TestCaseData> RestoreData = _restoreTemplates.Select(t => new TestCaseData(t));
