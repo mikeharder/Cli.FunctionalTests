01983245ee01178bc7222c94ed6a4e9343244262
Mike Harder <mharder@microsoft.com>
2018-08-10 11:10:55 -0700

    Update razor publish files for netcoreapp2.2

--- AspNetCoreSdkTests/Templates/RazorTemplate.cs
+++ AspNetCoreSdkTests/Templates/RazorTemplate.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using AspNetCoreSdkTests.Util;
+using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -12,19 +14,32 @@ namespace AspNetCoreSdkTests.Templates
 
         protected override string RazorPath => "Pages";
 
-        public override IEnumerable<string> ExpectedObjFilesAfterBuild => 
-            base.ExpectedObjFilesAfterBuild
-            .Concat(new[]
+        private IDictionary<string, Func<IEnumerable<string>>> _additionalFilesAfterPublish =>
+            new Dictionary<string, Func<IEnumerable<string>>>()
             {
-                Path.Combine("Razor", RazorPath, "_ViewStart.g.cshtml.cs"),
-                Path.Combine("Razor", RazorPath, "About.g.cshtml.cs"),
-                Path.Combine("Razor", RazorPath, "Contact.g.cshtml.cs"),
-                Path.Combine("Razor", RazorPath, "Error.g.cshtml.cs"),
-                Path.Combine("Razor", RazorPath, "Index.g.cshtml.cs"),
-                Path.Combine("Razor", RazorPath, "Privacy.g.cshtml.cs"),
-                Path.Combine("Razor", RazorPath, "Shared", "_CookieConsentPartial.g.cshtml.cs"),
-                Path.Combine("Razor", RazorPath, "Shared", "_Layout.g.cshtml.cs"),
-                Path.Combine("Razor", RazorPath, "Shared", "_ValidationScriptsPartial.g.cshtml.cs"),
-            }.Select(p => Path.Combine(OutputPath, p)));
+                { "netcoreapp2.1", () =>
+                    _additionalFilesAfterPublish["netcoreapp2.2"]()
+                    .Concat(new[]
+                    {
+                        Path.Combine("Razor", RazorPath, "About.g.cshtml.cs"),
+                        Path.Combine("Razor", RazorPath, "Contact.g.cshtml.cs"),
+                    })
+                },
+                { "netcoreapp2.2", () => new[]
+                    {
+                        Path.Combine("Razor", RazorPath, "_ViewStart.g.cshtml.cs"),
+                        Path.Combine("Razor", RazorPath, "Error.g.cshtml.cs"),
+                        Path.Combine("Razor", RazorPath, "Index.g.cshtml.cs"),
+                        Path.Combine("Razor", RazorPath, "Privacy.g.cshtml.cs"),
+                        Path.Combine("Razor", RazorPath, "Shared", "_CookieConsentPartial.g.cshtml.cs"),
+                        Path.Combine("Razor", RazorPath, "Shared", "_Layout.g.cshtml.cs"),
+                        Path.Combine("Razor", RazorPath, "Shared", "_ValidationScriptsPartial.g.cshtml.cs"),
+                    }
+                },
+            };
+
+        public override IEnumerable<string> ExpectedObjFilesAfterBuild =>
+            base.ExpectedObjFilesAfterBuild
+            .Concat(_additionalFilesAfterPublish[DotNetUtil.TargetFrameworkMoniker]().Select(p => Path.Combine(OutputPath, p)));
     }
 }
