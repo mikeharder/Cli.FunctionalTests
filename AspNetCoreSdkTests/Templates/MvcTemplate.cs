76ec3361da05b90ec45d65b58972056d5cf9005f
Mike Harder <mharder@microsoft.com>
2018-08-10 11:23:42 -0700

    Update mvc publish files for netcoreapp2.2

--- AspNetCoreSdkTests/Templates/MvcTemplate.cs
+++ AspNetCoreSdkTests/Templates/MvcTemplate.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using AspNetCoreSdkTests.Util;
+using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -12,19 +14,32 @@ namespace AspNetCoreSdkTests.Templates
 
         protected override string RazorPath => "Views";
 
+        private IDictionary<string, Func<IEnumerable<string>>> _additionalFilesAfterPublish =>
+            new Dictionary<string, Func<IEnumerable<string>>>()
+            {
+                { "netcoreapp2.1", () =>
+                    _additionalFilesAfterPublish["netcoreapp2.2"]()
+                    .Concat(new[]
+                    {
+                        Path.Combine("Razor", RazorPath, "Home", "About.g.cshtml.cs"),
+                        Path.Combine("Razor", RazorPath, "Home", "Contact.g.cshtml.cs"),
+                    })
+                },
+                { "netcoreapp2.2", () => new[]
+                    {
+                        Path.Combine("Razor", RazorPath, "_ViewStart.g.cshtml.cs"),
+                        Path.Combine("Razor", RazorPath, "Home", "Index.g.cshtml.cs"),
+                        Path.Combine("Razor", RazorPath, "Home", "Privacy.g.cshtml.cs"),
+                        Path.Combine("Razor", RazorPath, "Shared", "_CookieConsentPartial.g.cshtml.cs"),
+                        Path.Combine("Razor", RazorPath, "Shared", "_Layout.g.cshtml.cs"),
+                        Path.Combine("Razor", RazorPath, "Shared", "_ValidationScriptsPartial.g.cshtml.cs"),
+                        Path.Combine("Razor", RazorPath, "Shared", "Error.g.cshtml.cs"),
+                    }
+                },
+            };
+
         public override IEnumerable<string> ExpectedObjFilesAfterBuild =>
             base.ExpectedObjFilesAfterBuild
-            .Concat(new[]
-            {
-                Path.Combine("Razor", RazorPath, "_ViewStart.g.cshtml.cs"),
-                Path.Combine("Razor", RazorPath, "Home", "About.g.cshtml.cs"),
-                Path.Combine("Razor", RazorPath, "Home", "Contact.g.cshtml.cs"),
-                Path.Combine("Razor", RazorPath, "Home", "Index.g.cshtml.cs"),
-                Path.Combine("Razor", RazorPath, "Home", "Privacy.g.cshtml.cs"),
-                Path.Combine("Razor", RazorPath, "Shared", "_CookieConsentPartial.g.cshtml.cs"),
-                Path.Combine("Razor", RazorPath, "Shared", "_Layout.g.cshtml.cs"),
-                Path.Combine("Razor", RazorPath, "Shared", "_ValidationScriptsPartial.g.cshtml.cs"),
-                Path.Combine("Razor", RazorPath, "Shared", "Error.g.cshtml.cs"),
-            }.Select(p => Path.Combine(OutputPath, p)));
+            .Concat(_additionalFilesAfterPublish[DotNetUtil.TargetFrameworkMoniker]().Select(p => Path.Combine(OutputPath, p)));
     }
 }
