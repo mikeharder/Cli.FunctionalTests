f42c4cc74ce378073fe7d52319d0fda262591fda
Mike Harder <mharder@microsoft.com>
2018-08-13 12:09:24 -0700

    Add workaround to ReactRedux for 2.2

--- AspNetCoreSdkTests/Templates/ReactReduxTemplate.cs
+++ AspNetCoreSdkTests/Templates/ReactReduxTemplate.cs
@@ -1,9 +1,26 @@
-﻿namespace AspNetCoreSdkTests.Templates
+﻿using AspNetCoreSdkTests.Util;
+using System.IO;
+
+namespace AspNetCoreSdkTests.Templates
 {
     public class ReactReduxTemplate : ReactTemplate
     {
         public ReactReduxTemplate() { }
 
         public override string Name => "reactredux";
+
+        protected override void AfterNew(string tempDir)
+        {
+            // Workaround until https://github.com/aspnet/Templating/pull/672 is merged
+            const string before =
+@"<DistFiles Include=""$(SpaRoot)dist\**; $(SpaRoot)dist-server\**; $(SpaRoot)package.json"" />";
+
+            const string after =
+@"<DistFiles Include=""$(SpaRoot)dist\**; $(SpaRoot)dist-server\**"" />";
+
+            IOUtil.ReplaceInFile(Path.Combine(tempDir, $"{Name}.csproj"), before, after);
+
+            base.AfterNew(tempDir);
+        }
     }
 }
